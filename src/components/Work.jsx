7ab90b2742df1e55cf941e898e907dc8f27902ba
Kayla Hunt <huntkay1@msu.edu>
2024-10-18 20:05:26 -0400

    Add links to work cards

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,8 @@ import '../styles/Work.css'
 import Leafy from '../assets/leafy.png'
 import Pokemon from '../assets/pokemon.png'
 import Resume from '../assets/resume.png'
+import Sparkle from '../assets/sparkle.svg'
+import GitHub from '../assets/github.svg'
 import { NavLink } from 'react-router-dom';
 
 function Work() {
@@ -11,24 +13,24 @@ function Work() {
             name: 'LEAFY GREENS',
             description: 'Ecommerce site for a fictional plant shop',
             tech: ['React', 'CSS', 'Bootstrap', 'Express.js'],
-            website: '',
-            github: '',
+            website: 'https://poetic-starlight-e42a0f.netlify.app/',
+            github: 'https://github.com/huntkay1/shopping-cart',
             img: Leafy
         },
         {
-            name: 'POKEMON MEMORY GAME',
+            name: 'MEMORY GAME',
             description: 'Card game powered by the PokeAPI',
             tech: ['React', 'CSS'],
-            website: '',
-            github: '',
+            website: 'https://rad-bienenstitch-6bdaa4.netlify.app/',
+            github: 'https://github.com/huntkay1/memory-game-app',
             img: Pokemon
         }, 
         {
             name: 'RESUME BUILDER',
             description: 'Download a custom resume with the given template',
             tech: ['React', 'CSS'],
-            website: '',
-            github: '',
+            website: ' https://exquisite-kitsune-0ba888.netlify.app/',
+            github: 'https://github.com/huntkay1/cv-builder-app',
             img: Resume
         }, 
     ]
@@ -41,7 +43,7 @@ function Work() {
             <div className='work-container'>
                 {workData.map((item, index) => 
                     <div key={index} className='card'>
-                        <div className='card-info4'>
+                        <div className='card-info'>
                             <h3>{item.name}</h3>
                             <p className='hidden-content'>{item.description}</p>
                             <ul className='tech-list hidden-content'>
@@ -49,7 +51,18 @@ function Work() {
                                     <li key={index}>{tech}</li>
                                 )}
                             </ul>
+                            <div className='project-links'>
+                                <div className='hidden-content link'>
+                                    <img src={GitHub}></img>
+                                    <a href={item.github}>Repository</a>
+                                </div>
+                                <div className='hidden-content link'>
+                                    <img src={Sparkle}></img>
+                                    <a href={item.website}>Check it out</a>
+                                </div>
+                            </div>
                         </div>
+
                         <div className='card-img hidden-content' style={{backgroundImage: `url(${item.img})` }}></div>
 
                     </div>
@@ -59,7 +72,7 @@ function Work() {
             <div className='work-headers'>
                 <div className='work-copy'>
                     <p>This is a collection of the projects that I'm most proud of.</p>
-                    <p>I'm consistently adding fun new projects to this list as I keep experimenting and gain more skills!</p>
+                    <p>I'm consistently adding fun new projects to this list as I continue to experiment and level up my skills.</p>
                 </div>
                 <h2 className='page-header'>WORK</h2>
             </div>
