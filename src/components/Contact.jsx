3b07df4d1c0e4cc09fe71be4f65ef1d726abc649
Kayla Hunt <huntkay1@msu.edu>
2024-10-22 18:20:13 -0400

    Add contact content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,39 @@
 import '../styles/Contact.css'
+import { NavLink } from 'react-router-dom'
 
 function Contact() {
 
 
 
     return( 
-        <div>
-
+        <div className='contact'>
+            <NavLink to='/'>
+                <button className='home-button'>HOME</button>
+            </NavLink>
+            <h2 className='page-header'>Contact</h2>
+            <div className='form-container'>
+                <p>I'd love to hear from ya</p>
+                <form>
+                    <div className='input-block'>
+                        <label>NAME</label>
+                        <input type='text'></input>
+                    </div>
+                    <div className='input-block'>
+                        <label>NUMBER</label>
+                        <input type='tel'></input>
+                    </div>
+                    <div className='input-block'>
+                        <label>EMAIL</label>
+                        <input type='email'></input>
+                    </div>
+                    <div className='input-block'>
+                        <label>COMMENTS</label>
+                        <input type='text'></input>
+                    </div>
+                    <button type='submit'>SUBMIT</button>
+                </form>
+            </div>
+            
         </div>
     )
 }
