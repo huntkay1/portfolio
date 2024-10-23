b195f5e2a275fa89cd5ce407218d36409db65215
Kayla Hunt <huntkay1@msu.edu>
2024-10-23 18:27:05 -0400

    Set up email integration
    Utilized EmailJS to recieve emails sent
    from the contact form to my personal email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,45 +1,73 @@
-import '../styles/Contact.css'
-import { NavLink } from 'react-router-dom'
-import { useForm } from 'react-hook-form'
+import '../styles/Contact.css';
+import { NavLink } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
-   
-    const { register, handleSubmit, formState: {errors} } = useForm();
+  const form = useRef();
+  
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-    return( 
-        <div className='contact'>
-            <NavLink to='/'>
-                <button className='home-button'>HOME</button>
-            </NavLink>
-            <h2 className='page-header'>CONTACT</h2>
-            <div className='form-container'>
-                <form onSubmit={handleSubmit((data) => {
-                    console.log(data)
-                })}>
-                    <div className='input-block'>
-                        <div className='input-header'>
-                            <label>Name</label>
-                            <p className='error'>{errors.name?.message}</p>
-                        </div>
-                        <input {...register('name', {required: 'Please provide a name'})}type='text'></input>
-                    </div>
-                    <div className='input-block'>
-                        <div className='input-header'>
-                            <label {...register('email', {required: 'Please provide an email'})}>Email</label>
-                            <p className='error'>{errors.email?.message}</p>
-                        </div>
-                        <input type='email'></input>
-                    </div>
-                    <div className='input-block'>
-                        <label>Comments</label>
-                        <textarea {...register('comments')}></textarea>
-                    </div>
-                    <button type='submit'>Submit</button>
-                </form>
+  const sendEmail = (data, e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_4hgvw8i', 'template_fuz2glx', e.target, 'V9KKPMZIVttwL4GfW')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+
+    console.log(data); // This should contain the form data
+  };
+
+  return (
+    <div className="contact">
+      <NavLink to="/">
+        <button className="home-button">HOME</button>
+      </NavLink>
+      <h2 className="page-header">CONTACT</h2>
+      <div className="form-container">
+        <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+          <div className="input-block">
+            <div className="input-header">
+              <label>Name</label>
+              <p className="error">{errors.name?.message}</p>
             </div>
-            
-        </div>
-    )
+            <input 
+              {...register('user_name', { required: 'Please provide a name' })} 
+              type="text" 
+              name="user_name"
+            />
+          </div>
+          <div className="input-block">
+            <div className="input-header">
+              <label>Email</label>
+              <p className="error">{errors.email?.message}</p>
+            </div>
+            <input 
+              {...register('user_email', { required: 'Please provide an email' })} 
+              type="email" 
+              name="from_email"
+            />
+          </div>
+          <div className="input-block">
+            <label>Comments</label>
+            <textarea 
+              {...register('comments')} 
+              name="message"
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact 
+export default Contact;
