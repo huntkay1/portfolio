7dbea999638e3931b2fe40b5e870b6477a366523
Kayla Hunt <huntkay1@msu.edu>
2024-10-27 18:22:39 -0400

    Make about page mobile responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,14 @@ function About() {
                     Howdy. My name is Kayla and I'm a front-end developer (who dabbles in back-end) based in Michigan. 
                     </p>
                     <p>
-                    I have completed a bachelor's degree in Information Science, of which I had the experience to learn
+                    I have completed a bachelor's degree in Information Science, of which I had the experience of learning
                     the ins and outs of creating products for the web - including UX design & research, programming, and
                     web design. With this degree, I was inspired to continue on the path of web development, and have since
                     continued to teach myself many new skills including React & Node.js.  
                     </p>
                     <p>
                         One of the simple pleasures in life is coming across beautiful and easy-to-use websites. My goal is to craft 
-                        enjoyable experiences for users, as well as build sites that help you reach your goals. 
+                        enjoyable experiences for users while helping you reach your goals &#10023;
                     </p>
                 </div>  
 
